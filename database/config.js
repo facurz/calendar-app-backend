a1cc644946058f8488d049f86459d508e6c8cae7
Facundo Riva <facurz@gmail.com>
2023-03-16 11:11:08 -0300

    config mongoose strictQuery

--- database/config.js
+++ database/config.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const dbConnection = async () => {
     try {
+        mongoose.set('strictQuery', true)
         await mongoose.connect(process.env.DB_CNN);
         console.log('MongoDB Connected');
     } catch (err) {
--- index.js
+++ index.js
@@ -29,5 +29,5 @@ app.use('/api/events', require('./routes/events'))
 
 // Escuchar peticiones
 app.listen(process.env.PORT, ()=>{
-    console.log('Servidor corriendo en el puerto 4000')
+    console.log(`Servidor corriendo en puerto ${process.env.PORT}`)
 })
